685fed7c1f07ce24b408a8c0c44f77ee7fd40459
Yogesh Aswal <Yogesh@aswalyogesh29@gmail.com>
2024-04-07 22:48:24 +0530

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../component/Banner/Banner'
+import CoinTable from '../component/Banner/CoinTable'
+
+function HomePage() {
+  return (
+    <div>
+      <Banner/>
+      <CoinTable/>
+    </div>
+  )
+}
+
+export default HomePage
